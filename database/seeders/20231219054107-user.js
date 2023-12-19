c765b6480d7b0de9de565226b59e3997b70f0715
ARRARIAKU2 <yerry_kecak@yahoo.com>
2023-12-19 12:51:44 +0700

    Create users Seed

---
+++ database/seeders/20231219054107-user.js
@@ -0,0 +1,37 @@
+'use strict';
+const bcrypt = require('bcrypt');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    await queryInterface.bulkInsert('users', [{
+      username: 'admin',
+      email: 'admin@localhost.com',
+      password: bcrypt.hashSync('admin', bcrypt.genSaltSync(10)),
+      role: "superadmin",
+      created_at: new Date(),
+      updated_at: new Date()
+    }])
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+
+    await queryInterface.bulkDelete('users', null, {});
+  }
+};
