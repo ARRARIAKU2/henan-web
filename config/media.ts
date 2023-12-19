856ad14bb0741774b5af1297a3a22405b1e70f22
ARRARIAKU2 <yerry_kecak@yahoo.com>
2023-12-19 12:11:53 +0700

    Add Media handler setup

---
+++ config/media.ts
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from "cloudinary";
+import multer from "multer";
+
+class Media {
+    private upload;
+
+    constructor() {
+        this.upload = multer({ storage: multer.memoryStorage() });
+        cloudinary.config({
+            cloud_name: "dpmmsdjxy",
+            api_key: "647487294136952",
+            api_secret: "***************************",
+        });
+    }
+
+    get uploadMedia() {
+        return this.upload;
+    }
+
+    get storageMedia() {
+        return cloudinary;
+    }
+}
+
+export default new Media();
