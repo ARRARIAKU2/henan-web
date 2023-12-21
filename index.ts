ebfa9e47e3c8439a2adab367a1580dee7613d916
ARRARIAKU2 <yerry_kecak@yahoo.com>
2023-12-21 12:15:52 +0700

    Add Users Route

--- index.ts
+++ index.ts
@@ -1,23 +1,34 @@
 import express, { Express } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import ApiUsers from "./routes/users.route";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
 
 class Server {
-  private app: Express;
-  constructor() {
-    this.app = express();
+    private app: Express;
+    constructor() {
+        this.app = express();
 
-    this.app.use(express.json());
-    this.app.use(express.urlencoded({ extended: true }));
-  }
+        this.app.use(express.json());
+        this.app.use(cors());
+        this.app.use(express.urlencoded({ extended: true }));
 
-  run() {
-    try {
-      this.app.listen(3000, () => {
-        console.log("Server running on port 3000");
-      });
-    } catch (error) {
-      console.error("Error starting server:", error);
+        this.app.use("/api/users", ApiUsers.routes());
     }
-  }
+
+    run() {
+        try {
+            this.app.listen(PORT, () => {
+                console.log(`Server is running on http://localhost:${PORT}`);
+            });
+        } catch (error) {
+            console.error("Unable to connect to the Server:", error);
+        }
+    }
 }
 
 new Server().run();
